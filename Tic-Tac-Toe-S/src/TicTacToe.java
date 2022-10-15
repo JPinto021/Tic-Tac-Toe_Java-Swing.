c88b2a94ebbe08320822243e4e488d66981cebb6
Juan Carlos De Souza Pinto <juan.pintonlh@gmail.com>
2022-10-15 23:10:30 +0100

    Update TicTacToe.java
    familia Souza Pinto

--- Tic-Tac-Toe-S/src/TicTacToe.java
+++ Tic-Tac-Toe-S/src/TicTacToe.java
@@ -258,7 +258,7 @@ import javax.swing.*;
 		for(int i=0;i<9;i++) {
 			buttons[i].setEnabled(false);
 		}
-		textfield.setText("X Wins!!");  
+		textfield.setText("X familia souza Pinto ganhou!!");  
 
         }
 
